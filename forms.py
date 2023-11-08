657d425656621a36163a345df8e1b219f7a4deab
sayu349 <sayu10370@gmail.com>
2023-11-08 18:29:09 +0900

    forms.pyを作成しました

--- forms.py
+++ forms.py
@@ -1,17 +1,31 @@
 from wtforms import Form
 
-from wtforms.fields import StringField
-from wtforms.fields import IntegerField
-from wtforms.fields import PasswordField
-from wtforms.fields import DateField
-from wtforms.fields import RadioField
-from wtforms.fields import SelectField
-from wtforms.fields import BooleanField
-from wtforms.fields import TextAreaField
-from wtforms.fields import EmailField
-from wtforms.fields import SubmitField
+from wtforms.fields import (
+    StringField, IntegerField, PasswordField, DateField,
+    RadioField, SelectField, BooleanField, TextAreaField,
+    EmailField, SubmitField
+)
 
 class UserInfoForm(Form):
-    name = StringField("名前：", render_kw={"placeholder": "(例)山田 太郎"})
-
+    # 名前：文字列入力
+    name = StringField("名前：", render_kw={"placeholder":"(例)山田 太郎"})
+    # 年齢：静数値入力
+    age = IntegerField("年齢：", default=20)
+    # パスワード：パスワード入力
+    password = PasswordField("パスワード：")
+    # 確認用：パスワード入力
+    confirm_password = PasswordField("パスワード確認：")
+    # Email:メールアドレス
+    email = EmailField("メールアドレス：")
+    # 生年月日：日付入力
+    birthday = DateField("生年月日：", format="%Y-%m-%d", render_kw={"placeholder":"yyyy/mm/dd"})
+    # 性別：ラジオボタン
+    gender = RadioField("性別：", choices=[("man", "男性"),("women", "女性")], default="man")
+    # 出身地域：セレクトボックス
+    area = SelectField("出身地域：", choices=[("east","東日本"),("west","西日本")])
+    # 既婚：真偽値入力
+    is_married = BooleanField("既婚？：")
+    # メッセージ：複数行テキスト
+    note = TextAreaField("備考：")
+    # ボタン
     submit = SubmitField("送信")

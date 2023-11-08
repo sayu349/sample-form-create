b69ef5e00bd3aaa8173936153d8bcf2dfcd95325
sayu349 <sayu10370@gmail.com>
2023-11-08 17:12:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ from flask import render_template
 from flask import request
 
 from wtforms import Form
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["POST","GET"])
+def enter_form():
+    return render_template("index.html")

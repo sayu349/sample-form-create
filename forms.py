e426870497da3e87315b3e0efcb3337888720f66
sayu349 <sayu10370@gmail.com>
2023-11-10 22:58:58 +0900

    dbを構築しました。
    - dbを構築したことで、お問い合わせされたデータを保存することができます。
    - AppEngine上で実行されたときはクラウド環境に合わせた処理をするように、条件分岐してあります。

--- forms.py
+++ forms.py
@@ -6,29 +6,31 @@ from wtforms.fields import (
     SubmitField
 )
 # python3.11以上の場合
-#from wtforms.fields.html5 import EmailField
+from wtforms.fields.html5 import EmailField
 # python 3.11未満の場合
-from wtforms.fields import EmailField
+# from wtforms.fields import EmailField
 
-from wtforms.validators import DataRequired
+from wtforms.validators import (
+    DataRequired, Email, EqualTo
+)
 
 
 class UserInfoForm(Form):
     # 名前：文字列入力
-    name = StringField("お名前", render_kw={"placeholder":"(例)山田 太郎"})
+    name = StringField("お名前", render_kw={"placeholder":"(例)山田 太郎"}, validators=[DataRequired("お名前の入力は必須です")])
     # 年齢：静数値入力
     company = StringField("会社名", render_kw={"placeholder":"〇〇〇〇株式会社"})
     # お電話番号：文字列入力
     tel = StringField("お電話番号",render_kw={"placeholder":"012-3456-7890"})
     # メールアドレス：メールアドレス入力
-    email = EmailField("メールアドレス", render_kw={"placeholder":"xxxx@example.com"})
+    email = EmailField("メールアドレス", render_kw={"placeholder":"xxxx@example.com"}, validators=[EqualTo("confirm_password", "メールアドレスが一致しません")])
     # メールアドレス(確認用)：メールアドレス入力
-    confirm_email = EmailField("メールアドレス確認", render_kw={"placeholder":"xxxx@example.com"})
+    confirm_email = EmailField("メールアドレス確認", render_kw={"placeholder":"xxxx@example.com"}, validators=[Email("メールアドレスのフォーマットではありません")])
     # お問い合わせ概要：セレクトボックス
     inquiry = SelectField("お問い合わせ概要", choices=[("seminar", "セミナー・講演のご依頼"),("consultant", "相談・コンサルティングのご依頼"),("others","その他")])
     # お問い合わせ内容：テキストボックス
-    note = TextAreaField("お問い合わせ内容")
+    note = TextAreaField("お問い合わせ内容", validators=[DataRequired("お問い合わせ内容を入力してください")])
     # 同意：真偽値入力
-    privacy = BooleanField("プライバシーポリシーに同意する", validators=[DataRequired()])
+    privacy = BooleanField("プライバシーポリシーに同意する", validators=[DataRequired("プライバシーポリシーへの同意が必要です")])
     # ボタン
     submit = SubmitField("送信")
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+import os
+import sqlite3
 
 # インスタンス作成
 app = Flask(__name__)
@@ -26,18 +28,62 @@ def enter_form():
         email = form.email.data
         # お問い合わせ概要
         inquiry = form.inquiry.data
+        if inquiry=="seminar":
+            inquiry = "セミナー・講演のご依頼"
+        elif inquiry == "consultant":
+            inquiry = "相談・コンサルティングのご依頼"
+        else:
+            inquiry = "その他"
         # お問い合わせ内容
         note = form.note.data
 
-        # 出力
-        return render_template("thanks.html")
+        # 実行環境で条件分岐をする
+        if os.getenv('GAE_ENV', '').startswith('standard'):
+            # クラウド環境の場合
+            from google.cloud import storage
+            # GCS上のdbを取得する
+            client = storage.Client()
+            bucket_name = "sample-form-404507.appspot.com"
+            bucket = client.get_bucket(bucket_name)
+            blob_name = "sample-form-db/from.db"
+            blob = bucket.blob(blob_name)
+            blob.download_to_filename('/tmp/from.db')
+
+            # databaseにレコードを追加
+            con = sqlite3.connect('/tmp/from.db')
+            cur = con.cursor()
+            cur.execute('INSERT INTO form (name, company, tel, email, inquiry, note) VALUES (?, ?, ?, ?, ?, ?)',
+                        (name, company, tel, email, inquiry, note))
+            con.commit()
+            con.close()
+
+            # レコードを上書き保存する
+            client = storage.Client()
+            bucket_name = "sample-form-404507.appspot.com"
+            bucket = client.get_bucket(bucket_name)
+            blob_name = "sample-form-db/from.db"
+            blob = bucket.blob(blob_name)
+            blob.upload_from_filename('/tmp/from.db')
+
+        else:
+            # ローカル環境の場合
+            # databaseにレコードを追加
+            con = sqlite3.connect('database/form.db')
+            cur = con.cursor()
+            cur.execute('INSERT INTO form (name, company, tel, email, inquiry, note) VALUES (?, ?, ?, ?, ?, ?)',
+                        (name, company, tel, email, inquiry, note))
+            con.commit()
+            con.close()
+
+        return render_template("thanks.html",form = form)
 
     # GET:フォーム送信前
     else:
         return render_template("index.html",form=form)
 
 """
-# デバック時に使う
+# 本番環境では使わない！！
+# デバック時に使うこと
 if __name__== '__main__':
     app.run(debug=True)
 """
